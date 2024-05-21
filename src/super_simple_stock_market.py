9686628fa7f61b660ed37bb10d193775a7bddfe2
Adrian Garza <adrianmgarza@gmail.com>
2024-05-21 07:25:26 -0500

    adds calc pe ratio func and test

--- src/super_simple_stock_market.py
+++ src/super_simple_stock_market.py
@@ -23,8 +23,12 @@ class StockData:
 
 class SuperSimpleStockMarket:
     '''
-        This class has the 5 functions for the simple stock market
+        This class has the 5 functions for the super simple stock market
     '''
+    def _check_stock(self, stock):
+        stocks = StockData.stock_dividend_data['stock']
+        return True if stocks.isin([f'{stock}']).any() else False
+
     def calculate_dividend_yield(self, stock, price):
         '''
             This method calculates the dividend yield given a stock and price
@@ -33,7 +37,7 @@ class SuperSimpleStockMarket:
         :return:
         '''
         stocks = StockData.stock_dividend_data['stock']
-        if stocks.isin([f'{stock}']).any():
+        if self._check_stock(stock):
             stock_info = StockData.stock_dividend_data[stocks == f'{stock}']
         else:
             return f'The stock={stock} is not currently in the stock dividend dataset.'
@@ -44,13 +48,32 @@ class SuperSimpleStockMarket:
         if (stock_info['type'].astype('string') == 'Common').bool():
             dividend_yield = last_dividend / price
         else:
-            dividend_yield = None if np.isnan(fixed_dividend).bool() else (fixed_dividend * par_value) / price
+            dividend_yield = np.nan if np.isnan(fixed_dividend).bool() else np.round(((fixed_dividend * par_value) / price).iloc[0], decimals=2)
 
-        return dividend_yield.iloc[0]
+        return dividend_yield
+
+    def calculate_pe_ratio(self, stock, price):
+        '''
+            This function calculates the pe ratio given a stock and price
+        :param stock:
+        :param price:
+        :return:
+        '''
+        stocks = StockData.stock_dividend_data['stock']
+        if self._check_stock(stock):
+            stock_info = StockData.stock_dividend_data[stocks == f'{stock}']
+        else:
+            return f'The stock={stock} is not currently in the stock dividend dataset.'
+
+        dividend = stock_info['last_dividend']
+        pe = np.nan if np.isnan(dividend).bool() else np.round((price / dividend).iloc[0], decimals=2)
+        return pe
+
+    # def record_trade(self, stock, quantity, indicator, price):
+
 
 
 
 if __name__ == '__main__':
     sm = SuperSimpleStockMarket()
-    # print(sm.calculate_dividend_yield('AAPL', 200))
-    print(sm.calculate_dividend_yield('GIN', 100))
+    print(sm.calculate_pe_ratio('GIN', 100))
--- tests/unit/test_super_simple_stock_market.py
+++ tests/unit/test_super_simple_stock_market.py
@@ -10,3 +10,15 @@ def test_calculate_dividend_yield():
 
     dividend_yield = sm.calculate_dividend_yield('AAPL', 200)
     assert dividend_yield == 'The stock=AAPL is not currently in the stock dividend dataset.'
+
+
+def test_calculate_pe_ratio():
+    sm = super_simple_stock_market.SuperSimpleStockMarket()
+    pe = sm.calculate_pe_ratio('POP', 200)
+    assert pe == 25.00
+
+    pe = sm.calculate_pe_ratio('GIN', 100)
+    assert pe == 12.50
+
+    pe = sm.calculate_pe_ratio('AAPL', 500)
+    assert pe == 'The stock=AAPL is not currently in the stock dividend dataset.'
